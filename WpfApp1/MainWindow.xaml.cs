f524202d55fa6e423a8de64ccabb2fde1731d831
FlichZ <zubastik_bro@mail.ru>
2023-03-28 09:58:24 +0300

    END

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Notes
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            List<Note> notes = Serializer.Deserialize();    
+            if (notes == null)
+            {
+                notes = new List<Note>();
+            }
+            Date.SelectedDate = DateTime.Now;
+            Serializer.Serialize(notes);
+            ViewNotes();
+        }
+
+        private void SaveBtn_Click(object sender, RoutedEventArgs e)
+        {
+            List<Note> notes = Serializer.Deserialize();
+            try
+            {
+                notes[Notes_list.SelectedIndex - 1].name = Name.Text;
+                notes[Notes_list.SelectedIndex - 1].description = Description.Text;
+            }
+            catch
+            {
+                ShowErrorSelect();
+            }
+            Serializer.Serialize(notes);
+            ViewNotes();
+        }
+
+        private void CreateBtn_Click(object sender, RoutedEventArgs e)
+        {
+            List<Note> notes = Serializer.Deserialize();
+            if (notes == null)
+            {
+                notes = new List<Note>();
+            }
+            Note note = new Note();
+            note.name = Name.Text;
+            note.description = Description.Text;
+            note.date = DateTime.Parse(Date.Text);
+            notes.Add(note);
+            Serializer.Serialize(notes);
+            ViewNotes();
+        }
+
+        private void DeleteBtn_Click(object sender, RoutedEventArgs e)
+        {
+            List<Note> notes = Serializer.Deserialize();
+            try { 
+                notes.RemoveAt(Notes_list.SelectedIndex - 1);
+            }
+            catch
+            {
+                ShowErrorSelect();
+            }
+            Serializer.Serialize(notes);
+            ViewNotes();
+        }
+
+        private void ViewNotes()
+        {
+            List<Note> notes = Serializer.Deserialize();
+            List<String> CheckedNotes = new List<String>();
+            if (notes != null)
+            {
+                CheckedNotes.Add(" ");
+                foreach (Note note in notes)
+                {
+                    if (note.date == Date.SelectedDate)
+                    {
+                        CheckedNotes.Add(note.name);
+                    }
+                }
+                Notes_list.ItemsSource = CheckedNotes;
+                Description.Text = null;
+                Name.Text = null; 
+            }
+
+        }
+
+        private void ChangedDate(object sender, SelectionChangedEventArgs e)
+        {
+            ViewNotes();
+        }
+
+
+        private void Notes_list_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            List<Note> notes = Serializer.Deserialize();
+            try
+            {
+                Name.Text = notes[Notes_list.SelectedIndex - 1].name.ToString();
+                Description.Text = notes[Notes_list.SelectedIndex - 1].description.ToString();
+            }
+            catch { }
+        }
+
+        private void ShowErrorSelect()
+        {
+            MessageBox.Show("Нельзя выбирать пустой слот");
+        }
+    }
+}
---
+++ WpfApp1/Serializer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace Notes
+{
+    internal class Serializer
+    {
+        public static void Serialize(List<Note> notes)
+        {
+            string pathJson = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\notes.json";
+            string json = JsonConvert.SerializeObject(notes);
+            File.WriteAllText(pathJson, json);
+        }
+        public static List<Note> Deserialize()
+        {
+            try
+            {
+                string pathJson = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\notes.json";
+                string json = File.ReadAllText(pathJson);
+                List<Note> notes = JsonConvert.DeserializeObject<List<Note>>(json);
+                return notes;
+            }
+            catch
+            {
+                return new List<Note>();
+            }
+        }
+    }
+}
